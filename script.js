2c6aa8f9198d474d908fdb552282afea6c28ae02
AlvaroPDs <alvaropalmasilva@gmail.com>
2025-03-22 19:55:14 -0300

    update 2.1 adicionando html, css e js, ajuste de responsividade e contagem de tarefas

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const todoInputDescription = document.querySelector('#input-description');
 const todoInputTag = document.querySelector('#input-tag');
 const todoButton = document.querySelector('#add-task-btn');
 const todoList = document.querySelector('#todo-list');
-const listContainer = document.getElementById('todo-list');
+
 
 //Criadores de elemento
 const date = document.createElement('date')
 
-//Data de criação
+//Appends separados para elementos individuais
+
+
+//Data de criação de cada elemento da lista
 const d = new Date();
 const day = d.getDate();
 const month = d.getMonth();
@@ -17,13 +20,19 @@ const newDate = day + '/' + month + '/' + year;
 date.value = 'Criado em ' + newDate;
 
 //Event Listeners
-//CODIGO EXPERIMENTAL
+
+//(CODIGO EXPERIMENTAL)
 //document.addEventListener("DOMContentLoaded", getTodos);
+
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', buttonChecked);
+
+//(CODIGO EXPERIMENTAL)
 //todoList.addEventListener('click',)
 
 //Funções
+
+// Cria uma nova tarefa
 function addTodo(event){
     //Previne de ao dar submit ir pra outra pagina
     event.preventDefault();
@@ -31,35 +40,45 @@ function addTodo(event){
     //Div da to do list
     const todoDiv = document.createElement('div')
     todoDiv.classList.add('todo')
+    
     // Cria a lista
     const newTodo = document.createElement('li');
-    newTodo.innerHTML=`<div id="todo-info"><textarea readonly type="output" class="description">${todoInputDescription.value}</textarea><div  class ="sub-info-container"><p class="tag">${todoInputTag.value}</p><p class="date">${date.value}</p></div></div>`
+    newTodo.innerHTML=`<div id="todo-info"><textarea readonly name="desc" type="output" class="description">${todoInputDescription.value}</textarea><div  class ="sub-info-container"><p class="tag">${todoInputTag.value}</p><p class="date">${date.value}</p></div></div>`
     newTodo.classList.add('todo-item')
     todoDiv.appendChild(newTodo);
+    
     //(CODIGO EXPERIMENTAL)
     //saveLocalTodos(todoInputDescription.value)
     //saveLocalTodos(todoInputTag.value)
     //saveLocalTodos(date.value)
+    
     //Cria o botão de checado
     const completedButton = document.createElement('button')
     completedButton.classList.add('complete-btn')
     todoDiv.appendChild(completedButton)
+    
     //Cria o botão de apagar task
     const deleteTaskButton = document.createElement('button')
     deleteTaskButton.classList.add('trash-btn')
     deleteTaskButton.innerHTML='<i class="fa-solid fa-trash"></i>';
     todoDiv.appendChild(deleteTaskButton)
+
+    
     //Append a div para lista
     todoList.appendChild(todoDiv);
+    
     //Faz os inputs ficarem vazios após o submit
     todoInputDescription.value = [];
     todoInputTag.value = [];
+    
+    // Salva a nova tarefa no local storage
     saveData()
 }
 
-// Função do botão que altera a classe dos valores criados
+// Botão que altera a classe dos valores criados e apaga tarefas da lista
 function buttonChecked(e){
     const item = e.target;
+    
     //Deletar o item da lista
     if(item.classList[0]=== 'trash-btn'){
         const todo = item.parentElement;
@@ -69,24 +88,51 @@ function buttonChecked(e){
 
     //Botão checado
     if(item.classList[0] === 'complete-btn'){
-        // Esse codigo é experimental(não é o método mais simples mas funciona)
+        
+        // Seletores de elementos da lista
+        // (objetivo pra outro momento: "achar um codigo mais conciso que esse")
         const todoDescription = item.parentElement.children[0].children[0].children[0];
         const todoTag =  item.parentElement.children[0].children[0].children[1];
         const todoDate =  item.parentElement.children[0].children[0].children[1].children[1];
         const todoButtonCheck = item.parentElement.children[1];
+        
         // Trocas de classes
         todoDescription.classList.toggle('completed-line');
         todoTag.classList.toggle('completed-check');
         todoDate.classList.toggle('completed-check');
         todoButtonCheck.classList.toggle('checked')
-      
         
+    
     }
 
-    // Salva tudo no local storage
+    // Salva a execução dos botões no local storage
     saveData()
 
+    //Executa a função do contador de tarefas ao acionar o botão
+    taskProgressData()
+
 }
+
+//Contador de tarefas concluidas
+
+function taskProgressData(){
+    let tasksProgress
+
+    const tasksProgressDom = document.getElementById('tasks-progress')
+
+    if (tasksProgressDom) tasksProgress = tasksProgressDom;
+    else {
+        const newTasksProgressDOM = document.createElement('div');
+        newTasksProgressDOM.id = 'tasks-progress';
+        document.getElementById('todo-footer').appendChild(newTasksProgressDOM);
+        tasksProgress = newTasksProgressDOM;
+    }
+   
+    const doneTasks = document.getElementsByClassName('checked').length
+    tasksProgress.textContent = `${doneTasks} tarefa(s) concluída(s)`
+}
+
+//(CODIGO EXPERIMENTAL)
 //const invalidInput = (textinput) => {
 //    if(textinput.value === ''){
 //        textinput.setCustomValidity('Por favor preencha os dois campos!')
@@ -97,17 +143,25 @@ function buttonChecked(e){
 // Local storage
 
 function saveData(){
-  localStorage.setItem('data', listContainer.innerHTML)
+  localStorage.setItem('data', todoList.innerHTML)
 
 }
 
 function showData(){
-    listContainer.innerHTML = localStorage.getItem('data')
+    todoList.innerHTML = localStorage.getItem('data')
 }
-// Mostra tudo que foi salvo  no local storage
+
+//Mostra tudo que foi salvo  no local storage
 showData()
 
+window.onload = function (){
+    taskProgressData()
+}
+
+//Codigo pra limpar todos os items criados na lista
 //localStorage.clear()
+
+
 //(CODIGO EXPERIMENTAL)
 //function saveLocalTodos(todo){
 //    let todos;
